6978edf6a37557551390cad295e75347ea5c47a6
Katerina Popova <katerina_26popova@yahoo.com>
2022-03-16 23:24:21 +0100

    Add User App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserhandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserhandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
